684da5c7b2ceaaaa921c0ac883c013b5d908aa33
SamuelSam-dev <xsamansafaeix@gmail.com>
2022-08-02 01:56:34 +0430

    move toggleTheme.vue to header folder

--- components/home/header/ToggleTheme.vue
+++ components/home/header/ToggleTheme.vue
@@ -2,10 +2,10 @@
   <label class="flex items-center gap-2 cursor-pointer">
     <input type="checkbox" class="peer hidden" @input="switchTheme" :checked="$store.getters.getTheme === 'dark'" />
     <span
-      class="block w-10 h-6 rounded-full bg-gray-200 dark:bg-zinc-800
+      class="block w-10 h-6 rounded-full bg-zinc-800 transition-[background] duration-300
       relative after:content-[''] after:w-5 after:h-5
-      after:bg-purple-600 peer-checked:after:bg-zinc-500 after:rounded-full after:absolute
-      after:right-0.5 after:top-0.5 peer-checked:after:right-[1.125rem] after:transition-all"></span>
+      after:bg-purple-400 peer-checked:after:bg-zinc-500 after:rounded-full after:absolute
+      after:right-0.5 after:top-0.5 peer-checked:after:right-[1.125rem] after:transition-all after:ease-in-out after:duration-300"></span>
     <fa :icon="icon" class="text-gray-400" />
   </label>
 </template>
