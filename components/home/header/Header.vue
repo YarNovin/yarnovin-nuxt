772d344bbe508e29fe33e8a74a573d6454c8d75a
SamuelSam-dev <xsamansafaeix@gmail.com>
2022-08-02 01:55:58 +0430

    create a new component called header for index.vue

---
+++ components/home/header/Header.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="relative shadow-md dark:shadow-none bg-image z-0 pt-5">
+      <div class="absolute inset-0 -z-[1] bg-zinc-700/50 backdrop-blur-sm"></div>
+      <div class="z-10 text-white">
+        <HomeHeaderNavbar />
+        <HomeHeaderHead />
+      </div>
+    </div>
+</template>
