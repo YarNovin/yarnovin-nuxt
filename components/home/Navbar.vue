0b9181b08c3cea1e466b8bed293b4aef5710ba7b
SamuelSam-dev <xsamansafaeix@gmail.com>
2022-08-01 22:24:23 +0430

    upgrade navbar links animation

--- components/home/Navbar.vue
+++ components/home/Navbar.vue
@@ -7,9 +7,9 @@
       </div>
       <span class="grow"></span>
       <ul class="hidden sm:flex flex-row items-center h-full gap-1">
-        <li><a href="#groups" class="px-2.5 py-2 text-purple-900 dark:text-purple-200 rounded-md hover:bg-purple-100 dark:hover:bg-purple-100/10 border-solid border-b-2 border-transparent hover:border-b-purple-400 dark:hover:border-b-purple-200 transition-colors duration-500">گروه</a></li>
-        <li><a href="#services" class="px-2.5 py-2 text-purple-900 dark:text-purple-200 rounded-md hover:bg-purple-100 dark:hover:bg-purple-100/10 border-solid border-b-2 border-transparent hover:border-b-purple-400 dark:hover:border-b-purple-200 transition-colors duration-500">خدمات ما</a></li>
-        <li><a href="#team" class="px-2.5 py-2 text-purple-900 dark:text-purple-200 rounded-md hover:bg-purple-100 dark:hover:bg-purple-100/10 border-solid border-b-2 border-transparent hover:border-b-purple-400 dark:hover:border-b-purple-200 transition-colors duration-500">تیم</a></li>
+        <li><a href="#groups" class="relative px-2 py-2 text-purple-900 dark:text-purple-200 rounded-md hover:bg-purple-100 dark:hover:bg-purple-100/10 transition-colors duration-500 before:content-[''] before:absolute before:top-full before:right-0 before:w-0 hover:before:w-full before:h-0.5 before:bg-purple-700 dark:before:bg-purple-200 before:transition-all before:duration-300">گروه</a></li>
+        <li><a href="#services" class="relative px-2 py-2 text-purple-900 dark:text-purple-200 rounded-md hover:bg-purple-100 dark:hover:bg-purple-100/10 transition-colors duration-500 before:content-[''] before:absolute before:top-full before:right-0 before:w-0 hover:before:w-full before:h-0.5 before:bg-purple-700 dark:before:bg-purple-200 before:transition-all before:duration-300">خدمات ما</a></li>
+        <li><a href="#team" class="relative px-2 py-2 text-purple-900 dark:text-purple-200 rounded-md hover:bg-purple-100 dark:hover:bg-purple-100/10 transition-colors duration-500 before:content-[''] before:absolute before:top-full before:right-0 before:w-0 hover:before:w-full before:h-0.5 before:bg-purple-700 dark:before:bg-purple-200 before:transition-all before:duration-300">تیم</a></li>
       </ul>
       <span class="grow"></span>
       <GlobalToggleTheme />
