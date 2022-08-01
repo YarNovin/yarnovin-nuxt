0959f9b39d0e237879b010b0010ae3433ab74089
SamuelSam-dev <xsamansafaeix@gmail.com>
2022-08-02 02:09:00 +0430

    create a basic error page

---
+++ layouts/error.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <div v-if="error.statusCode === 404" class="min-h-[94vh] flex flex-col items-center justify-center gap-4">
+      <h3 class="text-2xl">صفحه مورد نظر شما یافت نشد</h3>
+      <nuxt-link to="/" class="text-purple-700 hover:text-purple-900 dark:text-purple-200 dark:hover:text-purple-50 transition-colors duration-300">بازگشت به صفحه اصلی</nuxt-link>
+    </div>
+  </div>
+</template>
+
+<script>
+import themeLoader from '~/mixins/themeLoader';
+
+export default {
+  mixins: [themeLoader],
+  props: ['error']
+}
+</script>
