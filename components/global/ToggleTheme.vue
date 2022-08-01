82a0402071202b0b0ed61a0b69ce9d5728f11dd7
SamuelSam-dev <xsamansafaeix@gmail.com>
2022-08-01 22:23:54 +0430

    use vuex store to load theme and switch theme

--- components/global/ToggleTheme.vue
+++ components/global/ToggleTheme.vue
@@ -1,10 +1,10 @@
 <template>
-  <label class="flex items-center gap-2">
-    <input type="checkbox" class="peer hidden" @input="switchTheme" />
+  <label class="flex items-center gap-2 cursor-pointer">
+    <input type="checkbox" class="peer hidden" @input="switchTheme" :checked="$store.getters.getTheme === 'dark'" />
     <span
       class="block w-10 h-6 rounded-full bg-gray-200 dark:bg-zinc-800
       relative after:content-[''] after:w-5 after:h-5
-      after:bg-amber-300 peer-checked:after:bg-zinc-500 after:rounded-full after:absolute
+      after:bg-purple-600 peer-checked:after:bg-zinc-500 after:rounded-full after:absolute
       after:right-0.5 after:top-0.5 peer-checked:after:right-[1.125rem] after:transition-all"></span>
     <fa :icon="icon" class="text-gray-400" />
   </label>
@@ -14,16 +14,24 @@
 export default {
   data() {
     return {
-      icon: ['fas', 'sun'],
       mode: 'light'
     }
   },
   methods: {
     switchTheme() {
-      document.documentElement.classList.toggle('dark')
-      this.mode === "light" ? this.mode = "dark" : this.mode = "light";
-      this.mode === "light" ? this.icon = ['fas', 'sun'] : this.icon = ['fas', 'moon']
-    },
+      this.$store.commit('switchTheme');
+    }
   },
+  computed: {
+    theme() {
+      return this.$store.getters.getTheme;
+    },
+    icon() {
+      return (this.$store.getters.getTheme === "dark") ? ['fas', 'moon'] : ['fas', 'sun'];
+    }
+  },
+  mounted() {
+    this.$store.commit("loadTheme");
+  }
 }
 </script>
