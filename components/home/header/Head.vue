a7960178dec2b17ce1a8e564c9ba844268bc3d40
SamuelSam-dev <xsamansafaeix@gmail.com>
2022-08-02 01:55:39 +0430

    rename header to Head

--- components/home/header/Head.vue
+++ components/home/header/Head.vue
@@ -1,10 +1,11 @@
 <template>
-  <header
+<div>
+<header
     id="about"
-    class="max-w-6xl w-full px-5 mx-auto flex flex-col md:flex-row md:items-center gap-x-14 gap-y-8"
+    class="max-w-6xl w-full px-7 py-20 mx-auto flex flex-col md:flex-row md:items-center gap-x-14 gap-y-8"
     dir="rtl">
     <div class="grow order-2 md:order-1">
-      <h1 class="text-3xl mb-3">یارنوین</h1>
+      <h1 class="text-5xl mb-6 font-bold">یار <span class="text-purple-300">نوین</span></h1>
       <p class="leading-8 text-justify mb-2">
         لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده
         از طراحان گرافیک است. چاپگرها و متون بلکه روزنامه و مجله در ستون و
@@ -24,4 +25,6 @@
         class="w-full rounded-lg drop-shadow-md" />
     </div>
   </header>
+</div>
+
 </template>
