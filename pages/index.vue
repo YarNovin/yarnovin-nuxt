b908e5b2d02d28ff90680aede8c9404f48eac77d
SamuelSam-dev <xsamansafaeix@gmail.com>
2022-08-02 01:56:45 +0430

    use header.vue in index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,20 +1,35 @@
 <template>
-<div>
-  <HomeNavbar />
-  <div class="flex flex-col items-stretch py-8 gap-8">
-    <HomeHeader/>
-    <section id="groups" class="max-w-6xl w-full p-5 mx-auto">
-      <h3 class="text-2xl text-center">گروه ها</h3>
-    </section>
+  <div>
+    <home-header/>
+    <div class="flex flex-col items-stretch py-8 gap-8">
+      <section id="groups" class="max-w-6xl w-full p-5 mx-auto">
+        <h3 class="text-2xl text-center">گروه ها</h3>
+      </section>
+    </div>
   </div>
-</div>
 </template>
 
 <script>
-import themeMixin from "~/mixins/themeLoader";
+import themeMixin from '~/mixins/themeLoader'
 
 export default {
   name: 'IndexPage',
-  mixins: [themeMixin]
+  mixins: [themeMixin],
+  head() {
+    return {
+      title: 'صفحه اصلی',
+      meta: [
+        {
+          name: 'description',
+          content: 'سایت مجموعه یارنوین . اطلاعات بیشتر در داخل سایت.',
+        },
+      ],
+    }
+  },
 }
 </script>
+<style scoped>
+.bg-image {
+  background: url("/images/head.jpg") center/cover no-repeat;
+}
+</style>
