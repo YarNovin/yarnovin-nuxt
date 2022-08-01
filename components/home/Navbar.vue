d698b708340be5a598598fb03a44cc9162eadc22
SamuelSam-dev <xsamansafaeix@gmail.com>
2022-08-01 15:59:45 +0430

    write classes better

--- components/home/Navbar.vue
+++ components/home/Navbar.vue
@@ -6,10 +6,10 @@
         <p>یارنوین</p>
       </div>
       <span class="grow"></span>
-      <ul class="flex flex-row items-center h-full gap-1">
-        <li><a href="#groups" class="text-purple-900 px-2.5 py-2 rounded-md dark:text-purple-200 hover:bg-purple-100 dark:hover:bg-purple-100/10 border-solid border-b-2 border-transparent hover:border-b-purple-400 dark:hover:border-b-purple-200 transition-colors duration-300">گروه</a></li>
-        <li><a href="#services" class="text-purple-900 px-2.5 py-2 rounded-md dark:text-purple-200 hover:bg-purple-100 dark:hover:bg-purple-100/10 border-solid border-b-2 border-transparent hover:border-b-purple-400 dark:hover:border-b-purple-200 transition-colors duration-300">خدمات ما</a></li>
-        <li><a href="#team" class="text-purple-900 px-2.5 py-2 rounded-md dark:text-purple-200 hover:bg-purple-100 dark:hover:bg-purple-100/10 border-solid border-b-2 border-transparent hover:border-b-purple-400 dark:hover:border-b-purple-200 transition-colors duration-300">تیم</a></li>
+      <ul class="hidden sm:flex flex-row items-center h-full gap-1">
+        <li><a href="#groups" class="px-2.5 py-2 text-purple-900 dark:text-purple-200 rounded-md hover:bg-purple-100 dark:hover:bg-purple-100/10 border-solid border-b-2 border-transparent hover:border-b-purple-400 dark:hover:border-b-purple-200 transition-colors duration-500">گروه</a></li>
+        <li><a href="#services" class="px-2.5 py-2 text-purple-900 dark:text-purple-200 rounded-md hover:bg-purple-100 dark:hover:bg-purple-100/10 border-solid border-b-2 border-transparent hover:border-b-purple-400 dark:hover:border-b-purple-200 transition-colors duration-500">خدمات ما</a></li>
+        <li><a href="#team" class="px-2.5 py-2 text-purple-900 dark:text-purple-200 rounded-md hover:bg-purple-100 dark:hover:bg-purple-100/10 border-solid border-b-2 border-transparent hover:border-b-purple-400 dark:hover:border-b-purple-200 transition-colors duration-500">تیم</a></li>
       </ul>
       <span class="grow"></span>
       <GlobalToggleTheme />
