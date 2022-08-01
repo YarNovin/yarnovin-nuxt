0e2577871d8957317c735fcc7c00751dba1dafaa
SamuelSam-dev <xsamansafaeix@gmail.com>
2022-08-02 02:13:29 +0430

    increase background opacity

--- components/home/header/Header.vue
+++ components/home/header/Header.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="relative shadow-md dark:shadow-none bg-image z-0 pt-5">
-      <div class="absolute inset-0 -z-[1] bg-zinc-700/50 backdrop-blur-sm"></div>
+      <div class="absolute inset-0 -z-[1] bg-zinc-700/70 backdrop-blur-[3px]"></div>
       <div class="z-10 text-white">
         <HomeHeaderNavbar />
         <HomeHeaderHead />
