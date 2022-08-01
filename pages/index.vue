b7e58725915361ec0b04a323432e523c10dcfdfc
SamuelSam-dev <xsamansafaeix@gmail.com>
2022-08-01 22:24:56 +0430

    create a responsive header and groups section

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,19 @@
 <template>
-  <div>hellow</div>
+  <div class="flex flex-col items-stretch py-8 gap-8">
+    <section id="about" class="max-w-6xl w-full px-5 mx-auto flex flex-col md:flex-row md:items-center gap-x-14 gap-y-8" dir="rtl">
+      <div class="grow order-2 md:order-1">
+        <h1 class="text-3xl mb-3">یارنوین</h1>
+        <p class="leading-8 text-justify mb-2">لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است. چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است و برای شرایط فعلی تکنولوژی مورد نیاز و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد. کتابهای زیادی در شصت و سه درصد گذشته، حال و آینده</p>
+        <p class="leading-8 text-justify">لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است.</p>
+      </div>
+      <div class="w-full md:w-5/12 shrink-0 order-1 md:order-2">
+        <img src="/images/yarnovin4.png" alt="brand-image" class="w-full rounded-lg drop-shadow-md">
+      </div>
+    </section>
+    <section id="groups" class="max-w-6xl w-full p-5 mx-auto">
+      <h3 class="text-2xl text-center">گروه ها</h3>
+    </section>
+  </div>
 </template>
 
 <script>
