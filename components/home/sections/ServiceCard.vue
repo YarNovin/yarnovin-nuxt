a4f4ca28cb9a433b5913be01f9645db2b708afa1
SamuelSam-dev <xsamansafaeix@gmail.com>
2022-08-09 14:42:19 +0430

    Create services and service card components

---
+++ components/home/sections/ServiceCard.vue
@@ -0,0 +1,68 @@
+<template>
+  <div>
+    <transition name="rotate" mode="out-in">
+      <div
+        key="preview"
+        @click="toggleCard"
+        v-if="!isCardOpen"
+        class="pers w-full h-full bg-gray-300 dark:bg-zinc-700 shadow-md rounded-2xl flex flex-col gap-4 justify-center items-center transition duration-500 select-none cursor-pointer">
+        <fa :icon="icon" class="w-10 h-10" />
+        <p class="text-xl text-center">{{ title }}</p>
+      </div>
+      <div
+        key="description"
+        v-if="isCardOpen"
+        @click="toggleCard"
+        dir="auto"
+        class="pers w-full h-full bg-gray bg-gray-300 dark:bg-zinc-700 shadow-md rounded-2xl p-4 flex justify-center items-center select-none cursor-pointer">
+        <p class="text-center">{{ bodyText }}</p>
+      </div>
+    </transition>
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    isCardOpen: false,
+  }),
+  methods: {
+    toggleCard() {
+      this.isCardOpen = !this.isCardOpen
+    },
+  },
+  props: {
+    icon: {
+      required: true,
+      type: Array,
+    },
+    title: {
+      required: true,
+      type: String,
+    },
+    bodyText: {
+      required: true,
+      type: String,
+    },
+  },
+}
+</script>
+<style scoped>
+.pers {
+  transform: perspective(400px);
+}
+
+.rotate-enter {
+  transform: rotateY(-90deg);
+  opacity: 0.2;
+}
+.rotate-leave-to {
+  transform: rotateY(90deg);
+  opacity: 0.2;
+}
+
+.rotate-enter-active,
+.rotate-leave-active {
+  transition: 0.4s all linear;
+}
+</style>
---
+++ components/home/sections/Services.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="grid grid-cols-1 md:grid-cols-3 w-full gap-6 lg:gap-8">
+    <div class="aspect-square">
+      <HomeSectionsServiceCard
+        class="w-full h-full"
+        title="ساخت ربات"
+        body-text="ساخت ربات های تلگرامی اختصاصی مخصوص گروه ها و کانال ها و همینطور ساخت ربات های واتساپ و اینستاگرام."
+        :icon="['fas', 'robot']" />
+    </div>
+    <div class="aspect-square">
+      <HomeSectionsServiceCard
+        class="w-full h-full"
+        title="ساخت صفحات وب"
+        body-text="ساخت صفحات رسپانسیو و همینطور با دیزاینی مناسب مخصوص کسب و کار شما."
+        :icon="['fas', 'brush']" />
+    </div>
+    <div class="aspect-square">
+      <HomeSectionsServiceCard
+        class="w-full h-full"
+        title="هوش مصنوعی"
+        body-text="نوشتن هوش مصنوعی برای مقاصد گوناگون مانند استفاده در ربات های تلگرامی و سایت های مختلف."
+        :icon="['fas', 'brain']" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
