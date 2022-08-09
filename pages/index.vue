4a649640e53cef5a75d56c85a1693e28fd81a8f9
SamuelSam-dev <xsamansafaeix@gmail.com>
2022-08-09 14:42:36 +0430

    upgrade some part sof site

--- pages/index.vue
+++ pages/index.vue
@@ -3,8 +3,8 @@
     <home-header/>
     <div class="flex flex-col items-stretch py-8 gap-8">
       <section id="services" class="max-w-6xl w-full p-5 mx-auto">
-        <h3 class="text-4xl text-center mb-4">خدمات ما</h3>
-        <HomeServices/>
+        <h3 class="text-4xl text-center mb-14">خدمات ما</h3>
+        <HomeSectionsServices />
       </section>
     </div>
   </div>
